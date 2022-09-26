014b39679b728c75e6fc5175e2957158fe79b89c
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2022-09-26 05:48:14 -0300

    Update telegram_notification.sh

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,35 +1,37 @@
 #!/bin/sh
 
-# Get the token from Travis environment vars and build the bot URL:
+# Get the token and build the bot URL:
 BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 
 # Set formatting for the message. Can be either "Markdown" or "HTML"
 PARSE_MODE="Markdown"
 
-# Use built-in Travis variables to check if all previous steps passed:
-if [ $GITHUB_ACTION_TEST_RESULT -ne 0 ]; then
-    build_status="failed"
-else
-    build_status="succeeded"
-fi
+# echo "${CI_JOB_STATUS}"
+
+echo "${CI_JOB_STATUS}"
+
+# echo "${https://t.me/graf_my_bot}"
+# echo "$TELEGRAM_TO"
+# echo "${TELEGRAM_TO}"
+
+# echo "${PARSE_MODE}"
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
-send_msg () {
-    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
-        -d text="$1" -d parse_mode=${PARSE_MODE}
-}
 
-# Send message to the bot with some pertinent details about the job
-# Note that for Markdown, you need to escape any backtick (inline-code)
-# characters, since they're reserved in bash
-send_msg "
+MESSAGE="
 -------------------------------------
-Travis build *${build_status}!*
-\`Repository:  ${dcoker-calcreact-app}\`
+GitHub Actions build * ${CI_JOB_STATUS}!*
+\`Repository:  ${docker-calcreact-app}\`
 \`Branch:      ${master}\`
 *Commit Msg:*
 ${Finalizado!}
-[Job Log here](${https://github.com/rosacarla/docker-calcreact-app/actions})
+)
 --------------------------------------
 "
+
+# echo "${MESSAGE}"
+# echo "$MESSAGE"
+
+curl -s -X POST ${https://t.me/graf_my_bot} -d chat_id=$TELEGRAM_TO -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
+
